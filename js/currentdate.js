7ae3be8ea4e9edaabc8c4dd32c383141ca514dc9
Spoyldrotn <spoyldrotnmama@gmail.com>
2019-09-23 16:48:36 -0600

    js current year

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,3 +1,3 @@
-let d = new Date(document.lastModified);
-let lastUpdate = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-document.getElementById('lastUpdate').innerHTML = lastUpdate;
+let today = new Date();
+let date = today.getFullYear();
+document.getElementById('currentdate'). innerHTML = date;
