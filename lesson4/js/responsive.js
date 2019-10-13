8aeac535c3c5bed323d2b5ae6ec6e6db219e7e76
Spoyldrotn <spoyldrotnmama@gmail.com>
2019-10-12 23:50:54 -0600

    fix js

--- lesson4/js/responsive.js
+++ lesson4/js/responsive.js
@@ -1,2 +1,2 @@
-function toggleMenu ();
-document.getElementsByClassName("navigation")[0].classList.toggle("responsive"); 
+function toggleMenu (){
+document.getElementsByClassName("navigation")[0].classList.toggle("responsive")}
