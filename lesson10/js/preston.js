b04dccf9366d7c9720ef167b337b867fbdc5ec25
Spoyldrotn <spoyldrotnmama@gmail.com>
2019-11-26 17:50:29 -0700

    typo

--- lesson10/js/preston.js
+++ lesson10/js/preston.js
@@ -1,22 +1,75 @@
 const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c317d9209bfb5e25c584b2191d32c8fa';
 
-//windchill//
+
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
         //console.log(jsObject);
 
-        var temp = parseFloat(jsObject.main.temp);
-        var speed = parseFloat (jsObject.wind.speed);
-        var chill = "NaN";
-            if (temp <= 50 && speed >3){
-                chill= 32.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 + temp + Math.pow (speed, 0.16)); 
-                chill.toFixed(2);
-            }
+        var temp = jsObject.main.temp.toFixed(0);
 
-                document.getElementById('currently').textContent = jsObject.weather[0].description;
-                document.getElementById('current-temp').textContent = temp;
-                document.getElementById('humidity').textContent = jsObject.main.humidity;
-                document.getElementById('windspeed').textContent = speed;
-                document.getElementById('windChill').textContent = chill;
-            });
+            document.getElementById('currently').textContent = jsObject.id.main;
+            document.getElementById('current-temp').textContent = temp;
+            document.getElementById('humidity').textContent = jsObject.main.humidity;
+            document.getElementById('windspeed').textContent = speed;
+
+
+
+            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';//note the concatenation
+            const desc = jsObject.weather[0].description;//notice how we reference the weather array
+            document.getElementById('weather').textContent=desc;
+    });
+
+    const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=c317d9209bfb5e25c584b2191d32c8fa';
+    
+    
+    fetch(jsonURL)
+        .then((response) => response.json())
+        .then ((jsObject) => {
+            //console.log(jsObject);
+
+            var day = "";
+            let count = 0;
+            for (let i = 0; i < jsObject.list.length; i++) {
+              if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+                count++;
+           
+                /*Temp*/
+                var temp = jsObject.list[i].main.temp.toFixed(0);
+        
+                let thehigh = 'high' + count;
+                document.getElementById(thehigh).textContent = temp;
+        
+                /*Day of Week*/
+        
+                let theday = 'day' + count;
+                var d = new Date();
+                var n = d.getDay();
+                n = n + count - 1;
+                if (n > 6){
+                  (n = n-7);
+                }
+        
+        
+        
+                if (n == 0) {
+                  day = "Sun";
+                } else if (n == 1) {
+                  day = "Mon";
+                } else if (n == 2) {
+                  day = "Tues";
+                } else if (n == 3) {
+                  day = "Wed";
+                } else if (n == 4) {
+                  day = "Thurs";
+                } else if (n == 5) {
+                  day = "Fri";
+                } else if (n == 6) {
+                  day = "Sat";
+                }
+        
+                document.getElementById(theday).textContent = day;
+
+              }
+            }
+          });
