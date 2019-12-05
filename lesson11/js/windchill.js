42fd34f955a21198a6811a6a81fbc4c1732f781f
Spoyldrotn <spoyldrotnmama@gmail.com>
2019-12-04 20:24:35 -0700

    fix error

--- lesson11/js/windchill.js
+++ lesson11/js/windchill.js
@@ -1,22 +1,19 @@
-function newChill(){
-    var temp = parseFloat(document.getElementById('high').innerHTML);
-    var speed = parseFloat(document.getElementById('speed').innerHTML);
-    var chill = windChill (temp, speed);
-    
-    
-    document.getElementById('windchill').innerHTML = chill;
+function windchill(weather, speed) {
+
+    let t = weather;
+    let s = speed;
+    let tMax = 50;
+    let sMin = 3;
+    let f = 0;
+
+    if (t < tMax && s > sMin) {
+        f = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
+        f = Math.round(f);
+
+        document.getElementById("windchill").innerHTML = f + "&#176;F";
+    } else {
+        f = "N/A";
+        document.getElementById("windchill").innerHTML = f;
     }
-    
-        function windChill(temp, speed){
-            var f = 0;
-            if (temp <= 50 && speed > 3) {  
-                f = 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed,0.16));
-                f = Math.round(f);
-            }
-            else{
-                f = temp;
-                f = Math.round(f);
-            }
-            return f;
-        }
-    
+    //console.log("Current Temp = " + t + ", Wind Speed = " + s + ", Wind Chill = " + f)
+}
